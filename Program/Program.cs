8f89ca1c9687f329fe5ab932ee915bc08635e79f
10vital08 <vital.lushnikov@mail.ru>
2022-09-15 20:21:37 +0300

    Добавил конечный код

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,9 +1,45 @@
-﻿string[] SplitTheString(string text)
+﻿//разбиение строки через разделитель
+string[] SplitTheString(string textAr)
 {
-    string[] resultArray = text.Split(";");
+    string[] resultArray = textAr.Split(";");
     return resultArray;
 }
 
-Console.WriteLine("Введите строки через " + ";");
+//коррекция строки по количеству символов(не более 3)
+string[] CorrectTheLine(string[] array)
+{
+    string[] correctLine = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            correctLine[i] = array[i];
+        }
+        else correctLine[i] = string.Empty;
+    }
+
+    correctLine = correctLine.Where(e => e != string.Empty).ToArray();//удалить элементы массива, равные пустой строке
+    return correctLine;
+}
+
+//распечатать строки
+void PrintLines(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"'{array[i]}' ");
+    }
+}
+
+Console.WriteLine("Введите строки через " + '"' + ";" + '"');
 string text = Console.ReadLine();
-SplitTheString(text);
+if (text != string.Empty)
+{
+    SplitTheString(text);
+    string[] textArray = SplitTheString(text);
+    string[] correctArray = CorrectTheLine(textArray);
+    PrintLines(correctArray);
+}
+else Console.WriteLine("Вы не заполнили строку");
+
+
