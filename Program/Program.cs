5433d972524a2aa1dfa7548910e3ae0fca428f3f
10vital08 <vital.lushnikov@mail.ru>
2022-09-15 18:31:54 +0300

    Добавил метод разделения строки и gitignore

---
+++ Program/Program.cs
@@ -0,0 +1,9 @@
+﻿string[] SplitTheString(string text)
+{
+    string[] resultArray = text.Split(";");
+    return resultArray;
+}
+
+Console.WriteLine("Введите строки через " + ";");
+string text = Console.ReadLine();
+SplitTheString(text);
